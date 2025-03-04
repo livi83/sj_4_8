0425157d90ab91bec74b20195aa9419df6bd50b0
Livia <livia.kelebercova@gmail.com>
2025-03-04 09:38:40 +0100

    Zaklady php

--- index.php
+++ index.php
@@ -9,6 +9,37 @@
     
         <?php
 
+            /* VÝPISY */
+            $meno = "Peter";
+            echo $meno;
+            echo '<br>';
+            //echo($meno);
+            var_dump($meno);
+            echo '<br>';
+
+            /* POLIA*/
+                            //0            1       2
+            $farby = array("červená", "modrá", "zelena"); //INDEXOVANÉ POLIA 
+            echo $farby[0]; // Vypíše: červená
+            echo("<br>");
+
+            /* CYKLUS NAD INDEXOVYNYM POLOM*/
+
+            foreach($farby as $farba){
+                echo "Farba: $farba <br>";
+                //echo "Farba: ".$farba."<br>";
+            }
+
+            /*ASOCIATIVNE POLIA*/
+            //python dictionary   kluc  :  hodnota
+            $data = array("Peter" => 25, 
+                        "Jana" => 30, 
+                        "Marek" => 20
+            );
+            
+            foreach($data as $name => $age){
+                echo "$name má $age rokov <br>";
+            }
         ?>
 </body>
 </html>
