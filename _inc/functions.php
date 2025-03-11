497ec622dee515fd2c3d38808fd46a524f06ced5
Livia <livia.kelebercova@gmail.com>
2025-03-11 09:59:11 +0100

    Funkcie - vypis menu

---
+++ _inc/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function get_menu(array $pages){
+    $menuItems = ''; 
+    foreach($pages as $page_name => $page_url){
+        // Pre každú stránku pridá odkaz do navigačného menu
+        $menuItems .= '<li><a href="' . $page_url . '">' . $page_name . '</a></li>';
+    }
+    // Vráti vygenerovaný HTML kód pre navigačné menu
+    return $menuItems;
+}
+
+?>
